b96479f41fb1ac49db0b54316e0d0f3005fb7ad1
Isaac Spencer <22ispencer@gmail.com>
2025-03-15 00:11:10 -0500

    initial commit basic stack operations

---
+++ easyrpn.c
@@ -0,0 +1,97 @@
+#include "easyrpn.h"
+#include <stdlib.h>
+
+// -- Calculator State --
+
+rpn_calc *rpn_init() {
+  rpn_calc *calc = malloc(sizeof(rpn_calc));
+  if (calc == NULL)
+    return NULL;
+
+  // initialize the stack
+  calc->stack.top = NULL;
+  calc->stack.bottom = NULL;
+  calc->stack.size = 0;
+
+  return calc;
+}
+
+void rpn_deinit(rpn_calc *calc) {
+  rpn_stacknode *prev_top = calc->stack.top;
+  while (prev_top != NULL) {
+    calc->stack.top = prev_top->prev;
+    free(prev_top);
+  }
+  free(calc);
+}
+
+rpn_stacknode *rpn_stack_empty_node() {
+  rpn_stacknode *node = malloc(sizeof(rpn_stacknode));
+  if (node == NULL)
+    return NULL;
+  node->next = NULL;
+  node->prev = NULL;
+  node->data = 0.0;
+  return node;
+}
+
+int rpn_stack_push(rpn_calc *calc, float num) {
+  rpn_stacknode *prev_top = calc->stack.top;
+  rpn_stacknode *top = rpn_stack_empty_node();
+  if (top == NULL) {
+    return -1;
+  }
+  top->data = num;
+
+  if (prev_top != NULL) {
+    top->prev = prev_top;
+    prev_top->next = top;
+  } else {
+    // If no items in the stack, add to bottom too
+    calc->stack.bottom = top;
+  }
+  calc->stack.top = top;
+  calc->stack.size++;
+
+  return 0;
+}
+
+float rpn_stack_drop(rpn_calc *calc) {
+  rpn_stacknode *prev_top = calc->stack.top;
+  float data = prev_top->data;
+
+  calc->stack.top = prev_top->prev;
+  if (prev_top->prev == NULL) {
+    calc->stack.bottom = NULL;
+  }
+  free(prev_top);
+  calc->stack.size--;
+
+  return data;
+}
+
+int rpn_stack_swap(rpn_calc *calc) {
+  rpn_stacknode *x = calc->stack.top;
+  if (x == NULL)
+    return -1;
+  rpn_stacknode *y = x->next;
+  if (y == NULL)
+    return -1;
+
+  x->prev = y->prev;
+  x->next = y;
+  y->prev = x;
+  y->next = NULL;
+
+  return 0;
+}
+
+void rpn_stack_roll(rpn_calc *calc) {
+  rpn_stacknode *top = calc->stack.top;
+  if (top == NULL || top->prev == NULL)
+    return;
+  calc->stack.top = top->prev;
+  top->next = calc->stack.bottom;
+  calc->stack.bottom = top;
+}
+
---
+++ include/easyrpn.h
@@ -0,0 +1,40 @@
+#ifndef LIBRPN_H
+#define LIBRPN_H
+
+// -- Calculator state --
+
+typedef struct rpn_stacknode {
+  float data;
+  struct rpn_stacknode *next;
+  struct rpn_stacknode *prev;
+} rpn_stacknode;
+
+typedef struct rpn_stack {
+  rpn_stacknode *top;
+  rpn_stacknode *bottom;
+  int size;
+} rpn_stack;
+
+typedef struct rpn_calc {
+  rpn_stack stack;
+} rpn_calc;
+
+/// Create a new calculator state
+rpn_calc *rpn_init();
+/// Destroy all app data, this frees `calc`
+void rpn_deinit(rpn_calc *calc);
+/// Add a number to the top of the stack
+int rpn_stack_push(rpn_calc *calc, float);
+/// Remove the top number in the stack
+float rpn_stack_drop(rpn_calc *calc);
+/// Swap the top two values on the stack
+int rpn_stack_swap(rpn_calc *calc);
+/// Roll the top value to the last value and move every other value forwards.
+void rpn_stack_roll(rpn_calc *calc);
+
+// -- Basic arithmetic functions
+
+/// Adds `num` to the top number on the stack
+int rpn_add(rpn_calc *calc);
+
+#endif // !LIBRPN_H
